e5eba5156464c20332bf02917cf3a546ce7a5468
SIN Pui Keung <130823389+kkkable@users.noreply.github.com>
2026-09-12 10:11:28 +0800

    feat: export completion confirmation localization

--- packages/localization/src/index.ts
+++ packages/localization/src/index.ts
@@ -1,6 +1,8 @@
 export { localizationCatalogs } from './catalogs.js';
 export type { LocalizationLocale, LocalizationMessageKey } from './catalogs.js';
 export { appTimeZoneUpdatedMessage } from './app-time-zone-messages.js';
+export { completionConfirmationCatalogs } from './completion-confirmation-catalogs.js';
+export type { CompletionConfirmationCatalog } from './completion-confirmation-catalogs.js';
 export { importedEventDisplayTitle } from './imported-event-title.js';
 export { progressLocalizationCatalogs } from './progress-catalogs.js';
 
