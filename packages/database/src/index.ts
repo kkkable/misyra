9753c9f82aad3e08ff84c920ccae72cd12333e3c
SIN Pui Keung <130823389+kkkable@users.noreply.github.com>
2026-09-13 20:25:35 +0800

    feat(mts-073): export hidden event store

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -13,4 +13,5 @@ export * from './device-settings-store.js';
 export * from './sync-store.js';
 export * from './google-calendar-connection-store.js';
 export * from './google-calendar-sync-store.js';
+export * from './google-calendar-hidden-event-store.js';
 export * from './google-calendar-watch-store.js';
